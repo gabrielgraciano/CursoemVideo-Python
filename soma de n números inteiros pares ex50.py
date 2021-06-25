0ee710a78bc5cd9b416d4acc9a6426079e49bb2a
gabrielgraciano <86334734+gabrielgraciano@users.noreply.github.com>
2021-06-25 18:12:37 -0300

    Update and rename soma dos números  inteiros pares ex50.py to soma de n números inteiros pares ex50.py

---
+++ soma de n números inteiros pares ex50.py
@@ -0,0 +1,9 @@
+# desenvolva um programa que leia n números inteiros e mostre a soma apenas dos pares. Desconsidere os impares
+s = 0
+k = int(input('até qual número você quer ir?'))
+for a in range(0,k):
+    b = int(input('me diga um número'))
+    if b % 2 ==0:
+        s += b
+print(f' a soma dos números é {s}')
+
--- soma dos números  inteiros pares ex50.py
+++
@@ -1,8 +0,0 @@
-# desenvolva um programa que leia seis números inteiros e mostre a soma apenas dos pares. Desconsidere os impares
-s = 0
-for a in range(0,6):
-    b = int(input('me diga um número'))
-    if b % 2 ==0:
-        s = 0 + b
-print(f' a soma dos números é {s}')
-
