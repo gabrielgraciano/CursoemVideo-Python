253d2f1dcb64f8d2d868547a9e5fd93840364960
gabrielgraciano <86334734+gabrielgraciano@users.noreply.github.com>
2021-07-03 23:13:17 -0300

    Update and rename ex055b.py to ex dos pesos resolução b.py

--- ex dos pesos resolução b.py
+++ ex dos pesos resolução b.py
@@ -1,7 +1,8 @@
 # faça um programa que leia o peso de n pessoas e diga qual deles é o mais pesado e qual é o mais leve
 maior = 0
 menor = 0
-for a in range(1,6):
+n = int(input('quantas pessoas são?'))
+for a in range(n):
     peso = float(input(f'qual é o peso da {a} pessoa?'))
     if a == 1:
         maior = peso
