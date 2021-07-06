2da2513e4e7882166f8222aa7dc73329736c17b2
gabrielgraciano <86334734+gabrielgraciano@users.noreply.github.com>
2021-07-06 17:55:18 -0300

    Create ex060 fatorial.py

---
+++ ex060 fatorial.py
@@ -0,0 +1,19 @@
+# faça um programa que leia um número qualquer e mostre o seu fatorial
+from math import factorial as f
+print('VAMOS CALCULAR FATORIAISSSSSSSSSSS')
+a = 0
+while a !=2:
+    a = int(input('''Qual das opções você quer?
+        [1] Calcular fatorial
+        [2] Sair do programa
+         '''))
+    if a != 2:
+        n = int(input('Digite o número do fatorial'))
+        if n < 0:
+            print('Não existe fatorial menor que 0')
+        elif n == 1:
+            print('O fatorial de 1 é 1')
+        else:
+            c = f(n)
+            print(f'O valor do fatorial em questão é {c}')
+print('Fim')
