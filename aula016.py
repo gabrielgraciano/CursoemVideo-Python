28aa14e42b281447aa2e8f9101d59a6931c7af80
gabrielgraciano <86334734+gabrielgraciano@users.noreply.github.com>
2021-07-19 23:23:34 -0300

    Create aula016.py

---
+++ aula016.py
@@ -0,0 +1,7 @@
+lanche = 'hambúguer', 'suco', 'pizza', 'pudim'
+for comida in range (0, len(lanche)):
+    print(lanche[comida])
+
+# ou também poderia ser
+# for comida in lanche:
+#   print(comida)
