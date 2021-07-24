bd9423eadb0ee08c48debe197087628ce035bd9a
gabrielgraciano <86334734+gabrielgraciano@users.noreply.github.com>
2021-07-24 17:25:17 -0300

    Create ex074 tupla de numeros aleatorios.py

---
+++ ex074 tupla de numeros aleatorios.py
@@ -0,0 +1,7 @@
+# crie um programa que vai gerar cinco numeros aleatorios e colocar em uma tupla. Depois mostre a listagem desses núme-
+# ros e também indique o maior e o menor número.
+from random import randint
+tupla = (randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 100),)
+for i in tupla:
+    print(i, end=' ')
+print(f'O maior valor é {max(tupla)} e o menor valor é {min(tupla)}')
