ceb675a8f2c4239ff757d179ad105cafa62048a7
gabrielgraciano <86334734+gabrielgraciano@users.noreply.github.com>
2021-07-28 02:43:24 -0300

    Create ex076.py

---
+++ ex076.py
@@ -0,0 +1,10 @@
+# crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
+# No final mostre uma listagem de preços, organizando os dados em forma tabular
+tupla = ('caderno', 15.00,
+         'lápis', 2.00,
+         'caneta', 1.00)
+for n in range(0, len(tupla)):
+    if n % 2 == 0:
+        print(f'{tupla[n]:.<30}', end=' ')
+    else:
+        print(f'R${tupla[n]:.2f}')
