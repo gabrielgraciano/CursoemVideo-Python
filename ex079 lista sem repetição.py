3b563b2898f105e14cec31c2296f094f851187aa
gabrielgraciano <86334734+gabrielgraciano@users.noreply.github.com>
2021-07-29 20:17:32 -0300

    Create ex079 lista sem repetição.py

---
+++ ex079 lista sem repetição.py
@@ -0,0 +1,20 @@
+''' Faça um programa onde o usuário pode inserir valores numéricos e cadastrar numa lista.
+Caso o número já exista lá, ele não será adicionado. No final serão exibidos todos os valores únicos digitados
+em ordem crescente '''
+lst = []
+
+while True:
+
+    a = int(input('Qual número você quer cadastrar? '))
+    if a not in lst:
+        lst.append(a)
+
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('você quer continuar? S/N ')).upper().strip()
+    if resp == 'N':
+        break
+
+print(lst)
+
+# '''por que eu não posso colocar resp = ''? dá erro'''
