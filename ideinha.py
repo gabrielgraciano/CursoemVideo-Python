42895ad974980ef4be22ed7024f47266cbcb8d6e
gabrielgraciano <86334734+gabrielgraciano@users.noreply.github.com>
2022-02-12 19:21:22 -0300

    Create ideinha.py

---
+++ ideinha.py
@@ -0,0 +1,12 @@
+#não sei muito bem o que fazer, mas queria um projeto novo em python
+import random
+min = int(input('dê é o menor valor possível para o número'))
+max = int(input('dê o maior valor possível para o número')) 
+numero = random.randint(min,max)
+while False:
+  guess = int(input('chute um número'))
+  if guess > numero:
+    print('o número dado é maior do que o número sorteado')
+  if guess < numero:
+    print('o numero dado é maior do que o número sorteado')
+  if guess  
