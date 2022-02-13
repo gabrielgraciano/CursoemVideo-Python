baa465806c28c27e61e75b0270805ecc94a2ca98
gabrielgraciano <86334734+gabrielgraciano@users.noreply.github.com>
2022-02-12 21:01:20 -0300

    Update ideinha.py

--- ideinha.py
+++ ideinha.py
@@ -4,9 +4,10 @@ min = int(input('dê é o menor valor possível para o número'))
 max = int(input('dê o maior valor possível para o número')) 
 numero = random.randint(min,max)
 while False:
-  guess = int(input('chute um número'))
-  if guess > numero:
-    print('o número dado é maior do que o número sorteado')
-  if guess < numero:
-    print('o numero dado é maior do que o número sorteado')
-  if guess  
+  try:
+    guess = int(input('chute um número'))
+    if guess > numero:
+      print('o número dado é maior do que o número sorteado')
+    if guess < numero:
+      print('o numero dado é maior do que o número sorteado')
+    
